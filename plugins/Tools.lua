95df6fc43a9ef51f44215fcf968362b92123a60d
asgharghasemi <aria_2007_tanha@yahoo.com>
2017-04-12 16:23:35 +0430

    aria

--- plugins/Tools.lua
+++ plugins/Tools.lua
@@ -1,5 +1,5 @@
 --Begin Tools.lua :)
-local SUDO = 304240834 -- put Your ID here! <===
+local SUDO = 140701941 -- put Your ID here! <===
 function exi_files(cpath)
     local files = {}
     local pth = cpath
